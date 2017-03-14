11ff5cc8219bfc73bc08859ad2499a961e489ab3
chris <chrismoss411@gmail.com>
2017-03-15 00:30:58 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,194 @@ declare var ledger: any;
 
 export class AppComponent {
 
+
+ledgerIndex = "44'/0'/0'/0";
+userAddress = "1Nh4tPtQjHZSoYdToTF7T3xbaKrTNKM3wP";
+sendAmount ="1.0";
+destinationAddress = "1gg14Fiz7uHoxAbAxkBaD2TYkFmGTu73Z";
   errorText = "";
  comm = ledger.comm_u2f;
 userBalance: Array<any>;
 
 constructor(private httpService:HTTPService){}
 
-public connect(){
 
+public send(i){
+ var self = this;
+
+ var tokenName = this.userBalance[i].token;
+ 
+document.getElementById("loading_send").style.display = "block";
+document.getElementById("send_"+tokenName).style.display = "none";
+
+document.getElementById("send_token").style.display = "none";
+document.getElementById("destination").style.display = "none";
+
+var sendAmountNum = parseFloat(this.sendAmount);
+
+console.log(" "+this.userAddress+" "+this.destinationAddress+" "+tokenName+" "+sendAmountNum);
+this.httpService.createSendTransaction(this.userAddress,this.destinationAddress,tokenName,sendAmountNum,10000,10000).subscribe(
+     data => {
+   
+   
+
+    var unsginedTX = data.unsigned_tx;
+    this.httpService.decodeRawTransaction(unsginedTX).subscribe(
+     data => {
+  console.log("decodedr:"+JSON.stringify(data));
+var decodedTransaction = data;
+
+   this.httpService.getRawTransactions(this.userAddress).subscribe(
+     data => {
+
+var currentInputs = decodedTransaction.vin;
+var numOfInputsFound = 0;
+for(var i = 0;i<data.length;i++){
+    var aUTXO = data[i];
+
+    for(var i2 = 0;i2<currentInputs.length;i2++){
+      var aCurrentInput = currentInputs[i2];
+      if(aUTXO.txid == aCurrentInput.txid){
+        aCurrentInput["txhex"] = aUTXO.hex;
+      }
+
+    }
+    
+
+}
+
+
+console.log("ci: "+JSON.stringify(currentInputs));
+
+
+
+
+
+
+this.comm.create_async().then(function(comm) {
+
+var btc = new ledger.btc(comm);
+var inputsArray = [];
+var keyPathArray = [];
+
+	for(var i = 0;i<currentInputs.length;i++){
+    var currentInput = currentInputs[i];
+  
+    var anInputObject = btc.splitTransaction(currentInput.txhex);
+    var inputIndex = currentInput.vout;
+    var inputObject = [];
+    inputObject.push(anInputObject);
+     inputObject.push(inputIndex);
+    inputsArray.push(inputObject);
+
+      keyPathArray.push(self.ledgerIndex);
+  }
+   
+
+
+  	var unsginedTxObject = btc.splitTransaction(unsginedTX);
+
+	var outputscript = btc.serializeTransactionOutputs(unsginedTxObject).toString("hex");
+
+	btc.createPaymentTransactionNew_async(inputsArray,keyPathArray, undefined, outputscript).then(function(result) {
+		
+    console.log(result);
+
+	//	document.getElementById("tx").innerHTML = result;
+
+	}).fail(function(ex) {console.log(ex);});
+
+
+
+}).fail(function(ex) {console.log(ex);});
+
+
+
+
+
+
+
+
+
+
+
+
+ document.getElementById("loading_send").style.display = "none";
+    document.getElementById("send_"+tokenName).style.display = "block";
+    document.getElementById("send_token").style.display = "block";
+document.getElementById("destination").style.display = "block";
+
+
+   },
+     error => {
+        console.log("decode error"+error);
+      
+    },
+     () => {});
+
+  
+
+
+/*
+
+*/
+
+
+
+
+   },
+     error => {
+        console.log("decode error"+error);
+      
+    },
+     () => {});
+
+  
+
+
+
+
+
+
+
+
+   },
+     error => {
+        console.log("error"+error);
+       this.errorText = error
+
+       document.getElementById("loading_send").style.display = "none";
+       document.getElementById("send_"+tokenName).style.display = "block";
+       document.getElementById("send_token").style.display = "block";
+document.getElementById("destination").style.display = "block";
+     
+    },
+     () => {});
+
+
+
+}
+
+public connect(){
+var self = this;
   document.getElementById("generate").style.display = "none"; 
 document.getElementById("loading").style.display = "block"; 
 
-   var httpServiceTmp = this.httpService;
- var errorTextTmp = this.errorText;
-var userBalanceTmp = this.userBalance;
-var self = this;
+
+  self.httpService.getBalance(self.userAddress).subscribe(
+     data => {self.userBalance = data;
+     document.getElementById("generate").style.display = "block"; 
+document.getElementById("loading").style.display = "none";    
+document.getElementById("send_token").style.display = "block";
+document.getElementById("destination").style.display = "block";
+},
+     error => {self.errorText = error},
+     () => {});
+
+
+/*
+ 
+
 
    this.comm.create_async().then(function(comm) {
 
@@ -32,28 +205,21 @@ var btc = new ledger.btc(comm);
 		
 		btc.getWalletPublicKey_async("44'/0'/0'/0").then(function(result) {
 		console.log(result.bitcoinAddress);
+self.userAddress = result.bitcoinAddress;
 
-
- httpServiceTmp.getBalance(result.bitcoinAddress).subscribe(
+  self.httpService.getBalance(self.userAddress).subscribe(
      data => {self.userBalance = data;
      document.getElementById("generate").style.display = "block"; 
 document.getElementById("loading").style.display = "none";    },
      error => {self.errorText = error},
-     () => {
- 
-  
-}
-     
-   );
+     () => {});
 
-
-		//document.getElementById("address").innerHTML = result.bitcoinAddress;
 	}).fail(function(ex) {
 document.getElementById("generate").style.display = "block"; 
 document.getElementById("loading").style.display = "none";  
 
 console.log(ex);
-document.getElementById("error").innerHTML = "error getting address"
+self.errorText = "error getting address"
 });
 
 
@@ -62,11 +228,11 @@ document.getElementById("error").innerHTML = "error getting address"
 document.getElementById("generate").style.display = "block"; 
 document.getElementById("loading").style.display = "none";
 console.log(ex);
-
+self.errorText = "error getting connecting to ledger"
 });
 
 
-
+*/
 }
 public runTest() {
 this.comm.create_async().then(function(comm) {
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -7,31 +7,81 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class HTTPService{
+    apiKey = "f29809821767e00b19e887e762e78e01";
   constructor(private _http:Http){}
+    
     getBalance(address){
-      return this._http.get("https://api.indiesquare.me/v2/addresses/"+address+"/balances")
+      return this._http.get("https://api.indiesquare.me/v2/addresses/"+address+"/balances?X-Api-Key=" + this.apiKey)
       .map(res=>res.json());
     };
 
-    postJson(){
-      var json = JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1
-      });
-      var params = 'data=' + json;
-      var header = new Headers();
-      header.append('Content-type', 'application/x-www-form-urlencoded');
+    broadcastTransaction(tx){
+       var json = JSON.stringify({
+             tx: tx,
+        });
 
-      return this._http.post("http://jsonplaceholder.typicode.com/posts",params, {
+      var params = json;
+      var header = new Headers();
+      header.append('Content-type', 'Content-Type: application/json');
+
+      return this._http.post("https://api.indiesquare.me/v2/transactions/broadcast?X-Api-Key=" + this.apiKey,params, {
         headers:header
       })
       .map(res => res.json());
     };
 
-    getUsersByPromise(){
-      return this._http.get("http://jsonplaceholder.typicode.com/users")
-      .toPromise()
-      .then(res=>res.json());
-    }
+    getRawTransactions(address){
+      return this._http.get("https://api.indiesquare.net/v2/addresses/"+address+"/rawtransactions?X-Api-Key=" + this.apiKey)
+      .map(res=>res.json());
+    };
+
+
+    decodeRawTransaction(tx){
+        
+      var json = JSON.stringify({
+             tx: tx,
+        });
+
+      var params = json;
+      var header = new Headers();
+      header.append('Content-type', 'Content-Type: application/json');
+
+      return this._http.post("https://api.indiesquare.me/v2/transactions/decode?X-Api-Key=" + this.apiKey,params, {
+        headers:header
+      })
+      .map(res => res.json());
+    };
+
+   
+
+
+    createSendTransaction(source,destination,token,quantity,fee_per_kb,fee){
+        if(fee != -1){
+            var json = JSON.stringify({
+              source: source,
+              destination: destination,
+              token: token,
+              quantity:quantity,
+              fee:fee,
+            });
+        }
+        else{
+          var json = JSON.stringify({
+              source: source,
+              destination: destination,
+              token: token,
+              quantity:quantity,
+              fee_per_kb:fee_per_kb,
+            });
+        }
+      var params = json;
+      var header = new Headers();
+      header.append('Content-type', 'Content-Type: application/json');
+
+      return this._http.post("https://api.indiesquare.me/v2/transactions/send",params, {
+        headers:header
+      })
+      .map(res => res.json());
+    };
+
 }

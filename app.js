afa0b10e6a25dde179043330297514b09d34b194
chris <chrismoss411@gmail.com>
2017-03-14 15:45:13 +0900

    na

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-app.use(express.static(_direname + '/app'));
-app.listen(process.env.PORT || 3000);
+ 
+app.set('port', (process.env.PORT || 5000));
+ 
+app.use(express.static(path.join(__dirname, 'dist')));
+ 
+// redireciona todas as requições para o Angular 2 
+app.all('*', function(req, res) {
+  res.status(200).sendFile(
+  	path.join(__dirname, 'dist', 'index.html'));
+});
+ 
+app.listen(app.get('port'), function() {
+  console.log('Node executando na porta ', app.get('port'));
+});

e19247d1b98bb6e0b2701781ffc1e1985765fcd3
chris <chrismoss411@gmail.com>
2017-03-17 14:15:19 +0900

    na

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var path = require('path');
-var express = require('express');
-var app = express();
- 
-app.set('port', (process.env.PORT || 5000));
- 
-app.use('/lib', express.static(__dirname + '/lib'));
-app.use(express.static(path.join(__dirname, 'dist')));
- 
+const path = require('path');
+const express = require('express');
+const app = express();
 
- // If an incoming request uses
+// If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
@@ -26,20 +20,16 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-
-// redireciona todas as requições para o Angular 2 
-app.all('*', function(req, res) {
-  res.status(200).sendFile(
-  	path.join(__dirname, 'dist', 'index.html'));
-});
- 
-app.listen(app.get('port'), function() {
-  console.log('Node executando na porta ', app.get('port'));
-});
-
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
